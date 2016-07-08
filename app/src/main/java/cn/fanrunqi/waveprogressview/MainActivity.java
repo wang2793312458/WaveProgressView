ce497b2a0527ecb41b11df3f92ef821749ae19c1
fanrunqi <fanrunqi@qq.com>
2016-07-08 09:36:12 +0800

    1.0

--- app/src/main/java/cn/fanrunqi/waveprogressview/MainActivity.java
+++ app/src/main/java/cn/fanrunqi/waveprogressview/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
-
         Init();
     }
 
